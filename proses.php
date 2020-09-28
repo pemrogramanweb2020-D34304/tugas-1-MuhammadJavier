3968810fcf39e6d9df6f4c2f8f5acb9c11ada332
MuhammadJavier <71283608+MuhammadJavier@users.noreply.github.com>
2020-09-28 23:32:35 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,25 @@
-
+<h2>
+<?php
+$bil1 = $_GET['bil1'];
+$bil2 = $_GET['bil2'];
+$operator = $_GET['operator'];
+echo "Bil 1 adalah $bil1 <br>";
+echo "Bil 2 adalah $bil2 <br>";
+if($operator == 1){
+    $jumlah = $bil1 + $bil2;
+    echo "Hasil Pertambahan Adalah $jumlah";
+}
+elseif($operator == 2){
+    $jumlah = $bil1 - $bil2;
+    echo "Hasil Pengurangan Adalah $jumlah";
+}
+elseif($operator == 3){
+    $jumlah = $bil1 * $bil2;
+    echo "Hasil Perkalian Adalah $jumlah";
+}
+elseif($operator == 4){
+    $jumlah = $bil1 / $bil2;
+    echo "Hasil Pembagian Adalah $jumlah";
+}
+?>
+</h2>
